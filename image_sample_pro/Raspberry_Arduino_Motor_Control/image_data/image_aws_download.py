f45088e1d2a73b0187381e3839e485539117cca8
miao.ma <miao.ma@bitmain.com>
2019-07-11 00:49:42 +0800

    Correct the logic of image_aws_download.py

--- image_sample_pro/Raspberry_Arduino_Motor_Control/image_data/image_aws_download.py
+++ image_sample_pro/Raspberry_Arduino_Motor_Control/image_data/image_aws_download.py
@@ -6,6 +6,7 @@ import sys
 import glob
 import time
 import sys
+import glob
 
 '''
 System Requir:
@@ -16,6 +17,7 @@ System Requir:
 
 mswindows = (sys.platform == "win32")
 linux = (sys.platform == "linux2" or sys.platform == "linux")
+macos = (sys.platform == "darwin")
 
 Upload_tm = time.localtime(time.time())
 Current_date = str(Upload_tm.tm_year) + '-' + str(Upload_tm.tm_mon) + '-' + str(Upload_tm.tm_mday) + '-' + str(Upload_tm.tm_hour) + '-' + str(Upload_tm.tm_min) + '-' + str(Upload_tm.tm_sec)
@@ -23,6 +25,8 @@ Current_date = str(Upload_tm.tm_year) + '-' + str(Upload_tm.tm_mon) + '-' + str(
 image_obj = []
 folder_obj = ['A1','A2','A3','A4','A5','A6','A7','A8','B1','B2','B3','B4','B5','B6','B7','B8',\
         'C1','C2','C3','C4','C5','C6','C7','C8','D1','D2','D3','D4','D5','D6','D7','D8']
+
+download_image_count = 0
 
 if mswindows:
     print('Platform:Windows.')
@@ -33,10 +37,11 @@ if mswindows:
         aws_cli = 'aws s3 cp s3://image-data/ ./' + folder + '/ --recursive --exclude \"*\" --include \"*_' + folder + '_V_NU.jpg\"'
         print('Downloading the ' + folder + '\'s image...')
         os.system(aws_cli)
+        download_image_count += 1
     Journal.close()
-    print("Finish Downloading Images.")
+    print("Finish Downloading " + str(download_image_count) + " Images.")
         
-if linux:
+if linux or macos:
     print('Platform:Linux.')
     print('Start downloading images:',Current_date)
     import boto3
@@ -48,13 +53,22 @@ if linux:
 
     for img in image_data_bucket.objects.all():
         image_name = img.key
-        print("Downloading " + image_name + "...")
         for folder_name in folder_obj:
-            if folder_name in image_name:
-                download_pos = './' + folder_name + '/' + image_name
-                Journal.write(image_name+'\n')
-                with open(download_pos,'ab') as data:
-                    image_data_bucket.download_fileobj(image_name,data)
-                break
+            if folder_name in image_name: # insert the code.
+                folder_image_dataset = glob.glob('./'+folder_name+'/*.jpg')
+                Need_Download = True
+                for folder_image_name in folder_image_dataset:
+                    if image_name in folder_image_name:
+                        print(image_name + ' is already exist!')
+                        Need_Download = False
+                        break
+                if Need_Download:
+                    print("Downloading " + image_name + "...")
+                    download_pos = './' + folder_name + '/' + image_name
+                    Journal.write(image_name+'\n')
+                    with open(download_pos,'ab') as data:
+                        image_data_bucket.download_fileobj(image_name,data)
+                    download_image_count += 1
+                    break
     Journal.close()
-    print("Finish Downloading Images.")
+    print("Finish Downloading " + str(download_image_count) + " Images.")
