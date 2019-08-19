133745c0e67f4f88e787c0a127ac726887502191
miao.ma <miao.ma@bitmain.com>
2019-08-19 13:57:13 +0800

    Add New Edition tempfile named imagesample.py

--- Library_Use_Ref/py_cpu_monitor.py
+++ Library_Use_Ref/py_cpu_monitor.py
@@ -5,33 +5,92 @@ import time
 import psutil
 
 #fans_Control pin initial
-fans_pin = 17
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(fans_pin,GPIO.OUT)
+
+heart_pin = 17
+
+def heart_led_init(PinNum):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PinNum,GPIO.OUT)
+def heart_led_blink(PinNum,speed):
+    GPIO.output(PinNum,False)
+    time.sleep(speed)
+    GPIO.output(PinNum,True)
+    time.sleep(speed)
+
+def HC595_init(PinNum1,PinNum2,PinNum3):
+    # print PinNum1,PinNum2,PinNum3
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PinNum1,GPIO.OUT)
+    GPIO.setup(PinNum2,GPIO.OUT)
+    GPIO.setup(PinNum3,GPIO.OUT)
+def HC595_SetState(data):
+    print(data)
 
 # Return CPU temperature as a character string                                     
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
-    return(res.replace("temp=","").replace("'C\n",""))
+    return float((res.replace("temp=","").replace("'C\n","")))
 
 def getCPUstate(interval=1):
-    return ("CPU:"+str(psutil.cpu_percent(interval)) + "%")
+    percent = psutil.cpu_percent(interval)
+    return percent,("CPU:"+str(percent) + "%")
 
 def getMemorystate():
     phymem = psutil.virtual_memory()
     line = "Memory: %5s%% %6s/%s"%(phymem.percent,str(int(phymem.used/1024/1024))+"M",str(int(phymem.total/1024/1024))+"M")
-    return line,phymem.percent
+    return phymem.percent,line
+
+def getAllstate(): # Temprature CPU-Percent Mem-Percent
+    Temp_cpu = getCPUtemperature()
+    Perc_cpu = getCPUstate()
+    Memo_cpu = getMemorystate()
+    return Temp_cpu,Perc_cpu[0],Memo_cpu[0]
+
+def process_heart_status(PinNum): # Need code the watchdog processing!
+    Emegency_Flag = {"CPU-Temp":0x01,"CPU-Perc":0x02,"CPU-Memo":0x04}
+    Emegency_Sate = 0x00
+    print("Thread to blink a led:blink if process alive.")
+    # heart_led_init()
+    blink_speed = 0.1
+    blink_count = 0
+    monitor_gap = 15 # Unit:s
+    while True:
+        Emegency_Sate = 0x00
+        Allstate = getAllstate()
+        print ("CPU-Temp/Perc/Memo",Allstate)
+        if Allstate[0] > 60:
+            Emegency_Sate |= Emegency_Flag["CPU-Temp"]
+        if Allstate[1] > 0.8:
+            Emegency_Sate |= Emegency_Flag["CPU-Perc"]
+        if Allstate[2] > 70:
+            Emegency_Sate |= Emegency_Flag["CPU-Memo"]
+        HC595_SetState(Emegency_Sate)
+        print('{TEMP:',Allstate[0],',PERC:',Allstate[1],',MEMO:',Allstate[2],'}')
+        blink_count = 0
+        blink_speed = (100.1 - Allstate[1])/100.0
+        print('Blink Speed:',blink_speed)
+        limit = monitor_gap/blink_speed/2
+        
+        while blink_count <= limit:
+            blink_count += 1
+            heart_led_blink(PinNum,blink_speed)
+
+
 
 T1 = time.time()
-Temp = getCPUtemperature()
-Temp_Value = float(Temp)
+Temp_Value = getCPUtemperature()
 CPU_percent = getCPUstate()
 Mem_State = getMemorystate()
 T2 = time.time()
 
-print "Time Cost:",(T2-T1)
-print "The Tempratrue is:",Temp_Value
-print "The CPU_percent:",CPU_percent
-print "The Memory_percent:",Mem_State[1],"%"
-print "The CPU Number:",psutil.cpu_count(logical=False)
+print("Time Cost:",(T2-T1))
+print("The Tempratrue is:",Temp_Value)
+print("The CPU_percent:",CPU_percent[0])
+print("The Memory_percent:",Mem_State[0],"%")
+print("The CPU Number:",psutil.cpu_count(logical=False))
+
+HC595_init(12,13,14)
+heart_led_init(heart_pin)
+process_heart_status(heart_pin)
---
+++ image_sample_pro/Raspberry_Arduino_Motor_Control/Raspberry_Arduino_ImageSample_temp.py
@@ -0,0 +1,373 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+import serial
+import time
+import os 
+
+Mashine_Name = 'P1'
+print 'Mashine_Name:',Mashine_Name
+print 'Raspberry GPIO initial...'
+
+Camera_IR_Pin = 17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Camera_IR_Pin,GPIO.OUT)
+
+def rpi2_usart_open(device,baudrate):
+    return serial.Serial(device,baudrate)
+def rpi2_usart_send(serial,data):
+    # serial.flushOutput()
+    serial.write(data)
+    # serial.flushOutput()
+def rpi2_usart_recv(serial,lenght):
+    buffers = ''
+    while True:
+        FIFO_len = serial.inWaiting()
+        # print "Waiting data:",FIFO_len
+        if lenght <= FIFO_len:
+            buffers = serial.read(FIFO_len)
+            # serial.flushInput()
+            break
+    return buffers
+
+CO2_dev = "/dev/ttyAMA0"
+CO2_baud = 9600
+
+def CO2_USART_Initial(CO2_dev,CO2_baud):
+    return rpi2_usart_open(CO2_dev,CO2_baud)
+def CO2_USART_GetValue(CO2_ser,CO2_CMD):
+    CO2_BUF = ''
+    rpi2_usart_send(CO2_ser,CO2_CMD)
+    CO2_BUF = rpi2_usart_recv(CO2_ser,7)
+                            
+    H_Bits = ord(CO2_BUF[3])
+    L_Bits = ord(CO2_BUF[4])
+                            
+    H_Bits_H = H_Bits>>4
+    H_Bits_L = H_Bits&0x0F
+    L_Bits_H = L_Bits>>4
+    L_Bits_L = L_Bits&0x0F
+                        
+    return H_Bits_H*4096 + H_Bits_L*256 + L_Bits_H*16 + L_Bits_L
+
+CO2_ser = CO2_USART_Initial(CO2_dev,CO2_baud)
+CO2_CMD = chr(254)+chr(4)+chr(0)+chr(3)+chr(0)+chr(1)+chr(213)+chr(197)
+
+x_n = 4
+y_n = 8
+
+x_distance = [0,58,100,100] # Unit:mm
+y_distance = [0,20,115,115,115,115,115,35] # [0,60,115,115,115,115,115,40]
+
+pre_min = 0
+min_update = 0
+
+print 'Initial the system...'
+
+x_axis_lable = 0
+y_axis_lable = 0
+
+Position = [['A1','B1','C1','D1'],['A2','B2','C2','D2'],['A3','B3','C3','D3']\
+        ,['A4','B4','C4','D4'],['A5','B5','C5','D5'],['A6','B6','C6','D6']\
+        ,['A7','B7','C7','D7'],['A8','B8','C8','D8']]
+
+
+Start_Date = time.localtime(time.time())
+
+if Start_Date.tm_mon >= 10:
+    Start_month = str(Start_Date.tm_mon)
+else:
+    Start_month = '0' + str(Start_Date.tm_mon)
+
+if Start_Date.tm_mday >= 10:
+    Start_day = str(Start_Date.tm_mday)
+else:
+    Start_day = '0' + str(Start_Date.tm_mday)
+
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+ser.flushInput()
+ser.flushOutput()
+Motor_CMD = dict({'x-right':'A','x-left':'B','y-down':'C','y-up':'D','x-init':'E','y-init':'F','stop':'S','Hold':'N'})
+System_CMD = dict({'CO2-ON':'G','CO2-OFF':'H'})
+print Motor_CMD,type(Motor_CMD),Motor_CMD['x-right']
+timeout_count = 0
+recv_data = 'N'
+recv_n = 0
+
+def Get_time_str(separator,struct=''):
+    if struct == '':
+        time_struct_val = time.localtime(time.time())
+        return str(time_struct_val.tm_year) + separator + str(time_struct_val.tm_mon) + separator + str(time_struct_val.tm_mday)+ separator + str(time_struct_val.tm_hour) + separator + str(time_struct_val.tm_min) + separator + str(time_struct_val.tm_sec)
+    return str(struct.tm_year) + separator + str(struct.tm_mon) + separator + str(struct.tm_mday)+ separator + str(struct.tm_hour) + separator + str(struct.tm_min) + separator + str(struct.tm_sec)
+
+abs_path = "/home/pi/nexgen_pro/image_sample_pro/Raspberry_Arduino_Motor_Control/image_sample_Journal.log" # ADD-line
+Journal = open(abs_path,'a')
+Journal.write("################################## System Start:" + Get_time_str(':') + " ###########################################\n")
+Journal.close()
+
+def ASK_Slave(serial,cmd_data):
+    global recv_data,recv_n
+    global abs_path
+    timeout_count = 0
+    timeout_total = 0
+    print 'recv_n',recv_n
+    print 'recv_data_B',recv_data
+    while True:
+        print "Motor is moving: ",timeout_count," times"
+        time.sleep(0.5)
+        recv_n = serial.inWaiting()
+        print cmd_data,'&& recv_n:',recv_n
+        if recv_n >= 2:
+            recv_data = serial.read(recv_n)
+            print recv_n,' && ',recv_data
+            serial.flushInput()
+            if 'OK' in recv_data and cmd_data[0] in recv_data:
+                print 'recv_data_A',recv_data,'+',recv_n
+                print 'Moving Finished.'
+                recv_n = 0
+                recv_data = 'N'
+                serial.flushOutput()
+                time.sleep(0.5) # 1
+                # Journal = open(abs_path,'a')
+                # Journal.write(cmd_data[0] + "->")
+                # Journal.close()
+                break
+            else:
+                print "Wrong Control,Break!"
+                recv_n = 0
+                recv_data = 'N'
+                serial.flushOutput()
+        timeout_count += 1
+        if timeout_count >= 600:
+            print "No response Slave,Repeat send cmd please."
+            serial.write(cmd_data)
+            timeout_total += timeout_count
+            timeout_count = 0
+            Journal = open(abs_path,'a')
+            if timeout_total <= 600:
+                Journal.write("\nSlave No response for CMD:" + cmd_data[0] + " " + str(timeout_total) + " times,Repeat Send CMD...\n")
+            else:
+                Journal.write("\nSlave No response for CMD:" + cmd_data[0] + " " + str(timeout_total) + " times,Shutdown System...\n")
+                Journal.write("System died at: " + Get_time_str(':') + '.\n')
+                os.system('sudo shudown -h now')
+            Journal.close()
+
+def Test_Motor(serial):
+    print 'Testing Motor...'
+    CMD_Send = Motor_CMD['x-left'] + chr(0)
+    serial.write(CMD_Send)
+    time.sleep(1.5)
+    serial.write(Motor_CMD['x-init'])
+    time.sleep(2)
+    CMD_Send = Motor_CMD['y-down'] + chr(0)
+    serial.write(CMD_Send)
+    time.sleep(1.5)
+    serial.write(Motor_CMD['y-init'])
+    time.sleep(2)
+    print 'Testing Motor Finished.'
+
+def Motor_Control(serial,CMD_IN,distance):
+    global Motor_CMD
+    CMD_Send = CMD_IN + chr(distance) + '\0'
+    print CMD_Send
+    if 'S' == CMD_Send[0]:
+        serial.write(Motor_CMD['stop'])
+        ASK_Slave(serial,Motor_CMD['stop'])
+        time.sleep(1.5)
+    else:
+        serial.write(CMD_Send)
+        ASK_Slave(serial,CMD_Send)
+        time.sleep(1)
+    time.sleep(1)
+
+def status_api(): # Socket communicate with other process and web server.
+    print "Try to use the other usart channle to communicate with Arduino and create a interrupt to deal with the CMD."
+    print "Listen to the AWS CMD.Get the global value of sensor and state of Delay-Controller and Update the state to AWS by boto3 lib."
+    print "Listen to the AWS CMD.Set the global value of sensor and state of Delay-Controller(mannully or automatic)"
+    print "Listen to the Local web page request,show the basic info."
+    print "Listen to the Local web page request,set the basic state."
+
+heart_pin = 12
+
+def heart_led_init(PinNum):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PinNum,GPIO.OUT)
+def heart_led_blink(PinNum,speed):
+    GPIO.output(PinNum,False)
+    time.sleep(speed)
+    GPIO.output(PinNum,True)
+    time.sleep(speed)
+
+def HC595_init(PinNum1,PinNum2,PinNum3):
+    # print PinNum1,PinNum2,PinNum3
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PinNum1,GPIO.OUT)
+    GPIO.setup(PinNum2,GPIO.OUT)
+    GPIO.setup(PinNum3,GPIO.OUT)
+def HC595_SetState(data):
+    print(data)
+
+# Return CPU temperature as a character string                                     
+def getCPUtemperature():
+    res = os.popen('vcgencmd measure_temp').readline()
+    return float((res.replace("temp=","").replace("'C\n","")))
+
+def getCPUstate(interval=1):
+    percent = psutil.cpu_percent(interval)
+    return percent,("CPU:"+str(percent) + "%")
+
+def getMemorystate():
+    phymem = psutil.virtual_memory()
+    line = "Memory: %5s%% %6s/%s"%(phymem.percent,str(int(phymem.used/1024/1024))+"M",str(int(phymem.total/1024/1024))+"M")
+    return phymem.percent,line
+
+def getAllstate(): # Temprature CPU-Percent Mem-Percent
+    Temp_cpu = getCPUtemperature()
+    Perc_cpu = getCPUstate()
+    Memo_cpu = getMemorystate()
+    return Temp_cpu,Perc_cpu[0],Memo_cpu[0]
+
+def process_heart_status(PinNum): # Need code the watchdog processing!
+    Emegency_Flag = {"CPU-Temp":0x01,"CPU-Perc":0x02,"CPU-Memo":0x04}
+    Emegency_Sate = 0x00
+    print("Thread to blink a led:blink if process alive.")
+    # heart_led_init()
+    blink_speed = 0.1
+    blink_count = 0
+    monitor_gap = 15 # Unit:s
+    while True:
+        Emegency_Sate = 0x00
+        Allstate = getAllstate()
+        print ("CPU-Temp/Perc/Memo",Allstate)
+        if Allstate[0] > 60:
+            Emegency_Sate |= Emegency_Flag["CPU-Temp"]
+        if Allstate[1] > 0.8:
+            Emegency_Sate |= Emegency_Flag["CPU-Perc"]
+        if Allstate[2] > 70:
+            Emegency_Sate |= Emegency_Flag["CPU-Memo"]
+        HC595_SetState(Emegency_Sate)
+        print('{TEMP:',Allstate[0],',PERC:',Allstate[1],',MEMO:',Allstate[2],'}')
+        blink_count = 0
+        blink_speed = (100.1 - Allstate[1])/100.0
+        print('Blink Speed:',blink_speed)
+        limit = monitor_gap/blink_speed/2
+        while blink_count <= limit:
+            blink_count += 1
+            heart_led_blink(PinNum,blink_speed)
+
+HC595_init(12,13,14)
+heart_led_init(heart_pin)
+# process_heart_status(heart_pin)
+
+print 'System Start Monitor...'
+while True:
+    date = time.localtime(time.time())
+    if pre_min != date.tm_min:
+        print "minutes update..."
+        if date.tm_hour >= 8 and date.tm_hour < 19: # Judge the CO2 detect time.
+            print "Monitor CO2..."
+            Time_Point = Get_time_str('-',struct=date) # ADD-line
+            CO2_PPM = CO2_USART_GetValue(CO2_ser,CO2_CMD) # Get the CO2-Values form Raspiberry<--->CO2_Sensor.
+            print Time_Point,':',CO2_PPM,'ppm'
+            if CO2_PPM < 1000: # Judge the CO2 ppm
+                Motor_Control(ser,'G',0); # Control the CO2-Delay status:ON
+            elif CO2_PPM > 1200:
+                Motor_Control(ser,'H',0); # Control the CO2-Delay status:OFF
+        else:
+            Motor_Control(ser,'H',0); # Control the CO2-Delay status:OFF
+        min_update = 1
+        pre_min = date.tm_min
+    if date.tm_hour % 1 == 0 and date.tm_min % 59 == 0 and min_update:
+        Motor_Control(ser,'H',0); # Control the CO2-Delay status:OFF
+        if date.tm_hour > 19 or date.tm_hour < 8: # Set the Camera's IR Capture func.
+            GPIO.output(Camera_IR_Pin,False)
+            print 'IR mode is ON!'
+        else:
+            GPIO.output(Camera_IR_Pin,True)
+            print 'IR mode is OFF!'
+        Time_Start = time.time()
+        Time_NOW = Get_time_str('-',struct=date) # ADD-line
+        Journal = open(abs_path,'a') # ADD-line
+        Journal.write("Start sampling images:"+Time_NOW+'\n') # ADD-line
+        Journal.close()
+        min_update = 0
+        print 'Current time:',Time_NOW
+        if date.tm_mon >= 10:
+            Current_month = str(date.tm_mon)
+        else:
+            Current_month = '0' + str(date.tm_mon)
+        if date.tm_mday >= 10:
+            Current_day = str(date.tm_mday)
+        else:
+            Current_day = '0' + str(date.tm_mday)
+        if date.tm_hour >= 10:
+            Current_hour = str(date.tm_hour)
+        else:
+            Current_hour = '0' + str(date.tm_hour)
+        if date.tm_min >= 10:
+            Current_min = str(date.tm_min)
+        else:
+            Current_min = '0' + str(date.tm_min)
+
+        print 'image sampling...'
+        Test_Motor(ser)
+        for x in range(x_n):
+            Motor_Control(ser,Motor_CMD['x-left'],x_distance[x]) # X-axis Going ON Positive
+            for y in range(y_n):
+                if x%2 == 0 and y == 0:
+                    Motor_Control(ser,Motor_CMD['y-init'],0)
+                if x%2 == 0 and y != 0:
+                    Motor_Control(ser,Motor_CMD['y-down'],y_distance[y])
+                    y_axis_lable += 1
+                if x%2 != 0 and y != 0:
+                    Motor_Control(ser,Motor_CMD['y-up'],y_distance[-y])
+                    y_axis_lable -= 1
+                print x_axis_lable,y_axis_lable
+                path = "/home/pi/nexgen_pro/image_sample_pro/Raspberry_Arduino_Motor_Control/image_data/"+Position[y_axis_lable][x_axis_lable]+'/'+Mashine_Name+'_'+str(Start_Date.tm_year)+Start_month+Start_day+'_'+str(date.tm_year)+Current_month+Current_day+'_'+Current_hour+Current_min+'_'+Position[y_axis_lable][x_axis_lable]+'_V'+'_NU'+'.jpg'
+                print 'Camera initilize...'
+                Camera_Status = 'BUSY'
+                while Camera_Status == 'BUSY':
+                    print "Try RPi-Camera..."
+                    try:
+                        camera = PiCamera()
+                        camera.resolution = (640,480)
+                        camera.framerate = 32
+                        camera.hflip = True
+                        camera.vflip = True
+                        camera.shutter_speed = 6000000
+                        camera.iso = 20
+                        camera.saturation = 0
+                        camera.brightness = 50
+                        camera.sharpness = 0
+                        time.sleep(1)
+                        camera.capture(path,use_video_port = False)
+                        camera.close()
+                        print 'Sample Successfully.'
+                        Camera_Status = 'FREE'
+                        pass
+                    except Exception,e:
+                        print 'Camera Wrong!'
+                        Camera_Status = 'BUSY'
+                        Error_tm_str = Get_time_str(':')
+                        Journal = open(abs_path,'a')
+                        Journal.write(Error_tm_str + "-Camera Wrong? "+str(Exception)+':'+str(e)+'\n')
+                        Journal.close()
+                        time.sleep(5)
+                        pass
+                Journal = open(abs_path,'a')
+                Journal.write(path[83:]+'\n') # ADD-line
+                Journal.close()
+                print path
+            x_axis_lable += 1
+        Motor_Control(ser,Motor_CMD['x-init'],0)
+        x_axis_lable = 0
+        y_axis_lable = 0
+        Time_End = time.time()
+        TimeCost = (Time_End-Time_Start)/60
+        print '################ Time cost: ',TimeCost,' minutes. ####################'
+        Journal = open(abs_path,'a')
+        Journal.write("Sample Finished.(TimeCost:"+str(TimeCost)+" mins)\n")
+        Journal.close() # ADD-line
