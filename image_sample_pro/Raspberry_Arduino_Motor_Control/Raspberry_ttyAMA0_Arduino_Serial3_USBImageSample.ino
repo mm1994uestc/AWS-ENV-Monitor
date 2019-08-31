e01b8e8c45235498c73e770b499b13954146c4fe
miao.ma <miao.ma@bitmain.com>
2019-08-31 23:00:51 +0800

    New Release Edition.Almost all the Bug fixed.

---
+++ image_sample_pro/Raspberry_Arduino_Motor_Control/Raspberry_ttyAMA0_Arduino_Serial3_USBImageSample.ino
@@ -0,0 +1,350 @@
+#include <GravityTDS.h>
+#include <Wire.h>
+
+#define ADDRESS_SI7021 0x40
+#define MEASURE_RH_HOLD 0xE5
+#define READ_T_FROM_PRE_RH_MEASUREMENT 0xE0
+
+#define CMD_DATA_Len 2
+
+const int PH_SensorPin = A14;
+float Current_PH = 0;
+
+const int EC_SensorPin = A15;
+GravityTDS gravityTds;
+float temperature = 25,Current_EC = 0;
+
+float Humi = 0.0;
+float Temp = 0.0;
+
+#define x_mm_pp 0.0711
+#define y_mm_pp 0.0625// 1:100=0.0625 1:50=0.125
+
+int Update_Flag = 0;
+char Buffers[CMD_DATA_Len] = {0};
+
+#define CMD_len 8
+int Current_CO=0;
+unsigned char CMD_Array[8] = {0xFE,0x04,0x00,0x03,0x00,0x01,0xD5,0xC5};
+
+const int Pin_AP = 2;
+const int Pin_AN = 3;
+const int Pin_BP = 4;
+const int Pin_BN = 5;
+const int Y_Trigger = 6;
+const int Y_Speed_us = 1800; //1800 us Unit:us
+
+const int dirPin = 8;
+const int stepperPin = 7;
+const int enPin = 9;
+const int X_Trigger = 11;
+const int X_Speed = 800;
+
+unsigned int x_abs_position = 0;
+unsigned int y_abs_position = 0;
+int current_steps = 0;
+
+#define Delay_Device 7
+const int CO2_Control_Pin = 18,whiteLight_Pin = 19,GorwLight1_Pin = 24,GorwLight2_Pin = 25,DosingPump1_Pin = 22,DosingPump2_Pin = 23,DosingPump3_Pin = 26;
+const int Delay_PinList[Delay_Device] = {CO2_Control_Pin,whiteLight_Pin,GorwLight1_Pin,GorwLight2_Pin,DosingPump1_Pin,DosingPump2_Pin,DosingPump3_Pin};
+void DelayPin_initial(int *PinList,int len)
+{
+  int i=0;
+  for(i=0;i<len;i++){
+    pinMode(PinList[i],OUTPUT);
+  }
+}
+void DelayPin_Control(int PinNum,int state)
+{
+  digitalWrite(PinNum,state);
+}
+
+void x_step(boolean dir,int steps)
+{
+  digitalWrite(enPin,0);
+  digitalWrite(dirPin,dir);
+  delay(10);
+  for(int i=0;i<steps;i++){
+    digitalWrite(stepperPin, HIGH);
+    delayMicroseconds(X_Speed);
+    digitalWrite(stepperPin, LOW);
+    delayMicroseconds(X_Speed); 
+  }  
+  
+  if(dir == 0) x_abs_position += steps;
+  if(dir == 1) x_abs_position -= steps;
+}
+void y_step(boolean dir,int steps)
+{
+  if(dir==0)
+  {
+    for(int i=0;i<steps;i++){
+      digitalWrite(Pin_AP,HIGH); digitalWrite(Pin_AN,LOW);
+      digitalWrite(Pin_BP,LOW);  digitalWrite(Pin_BN,HIGH);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,HIGH); digitalWrite(Pin_AN,LOW);
+      digitalWrite(Pin_BP,HIGH); digitalWrite(Pin_BN,LOW);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,LOW);  digitalWrite(Pin_AN,HIGH);
+      digitalWrite(Pin_BP,HIGH); digitalWrite(Pin_BN,LOW);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,LOW); digitalWrite(Pin_AN,HIGH);
+      digitalWrite(Pin_BP,LOW);  digitalWrite(Pin_BN,HIGH);
+      delayMicroseconds(Y_Speed_us);
+      y_abs_position -= 1;
+    }
+  }
+  if(dir==1)
+  {
+    for(int i=0;i<steps;i++){
+      digitalWrite(Pin_AP,LOW); digitalWrite(Pin_AN,HIGH);
+      digitalWrite(Pin_BP,LOW);  digitalWrite(Pin_BN,HIGH);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,LOW);  digitalWrite(Pin_AN,HIGH);
+      digitalWrite(Pin_BP,HIGH); digitalWrite(Pin_BN,LOW);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,HIGH); digitalWrite(Pin_AN,LOW);
+      digitalWrite(Pin_BP,HIGH); digitalWrite(Pin_BN,LOW);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,HIGH); digitalWrite(Pin_AN,LOW);
+      digitalWrite(Pin_BP,LOW);  digitalWrite(Pin_BN,HIGH);
+      delayMicroseconds(Y_Speed_us);
+      y_abs_position += 1;
+    }
+  }
+  digitalWrite(Pin_AP,0); digitalWrite(Pin_AN,0); // Close the micro motor
+  digitalWrite(Pin_BP,0); digitalWrite(Pin_BN,0);
+}
+
+void x_initial(void)
+{
+  digitalWrite(enPin,0);
+  digitalWrite(dirPin,1);
+  x_step(1,x_abs_position); // We first control the motor back itself,and then we use the IR-Sensor to check it.
+  while(digitalRead(X_Trigger) == 0){
+    digitalWrite(stepperPin, HIGH);
+    delayMicroseconds(X_Speed);
+    digitalWrite(stepperPin, LOW);
+    delayMicroseconds(X_Speed-20);
+  }
+  digitalWrite(enPin,1); // Close the 42Motor
+  x_abs_position = 0;
+}
+
+void y_initial(void)
+{
+  y_step(0,y_abs_position); // We first control the motor back itself,and then we use the IR-Sensor to check it.
+  /*
+  while(digitalRead(Y_Trigger) == 1){ // Judge the condition cost the time.We define it about 20us.
+      digitalWrite(Pin_AP,HIGH); digitalWrite(Pin_AN,LOW);
+      digitalWrite(Pin_BP,LOW);  digitalWrite(Pin_BN,HIGH);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,HIGH); digitalWrite(Pin_AN,LOW);
+      digitalWrite(Pin_BP,HIGH); digitalWrite(Pin_BN,LOW);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,LOW);  digitalWrite(Pin_AN,HIGH);
+      digitalWrite(Pin_BP,HIGH); digitalWrite(Pin_BN,LOW);
+      delayMicroseconds(Y_Speed_us);
+      digitalWrite(Pin_AP,LOW); digitalWrite(Pin_AN,HIGH);
+      digitalWrite(Pin_BP,LOW);  digitalWrite(Pin_BN,HIGH);
+      delayMicroseconds(Y_Speed_us-20);
+  }*/
+  digitalWrite(Pin_AP,0); digitalWrite(Pin_AN,0); // Close the micro motor
+  digitalWrite(Pin_BP,0); digitalWrite(Pin_BN,0);
+  y_abs_position = 0;
+}
+
+void Stop_Motor(void)
+{
+  digitalWrite(Pin_AP,LOW); digitalWrite(Pin_AN,LOW); digitalWrite(Pin_BP,LOW); digitalWrite(Pin_BN,LOW); digitalWrite(enPin,1);
+}
+
+float Get_PH_Val(const int PH_SensorPin)
+{
+  unsigned long int avgValue;  //Store the average value of the sensor feedback
+  float b;
+  int buf[10],temp;
+  for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+  { 
+    buf[i]=analogRead(PH_SensorPin);
+    delay(5);
+  }
+  for(int i=0;i<9;i++)        //sort the analog from small to large
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+    avgValue+=buf[i];
+  float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+  phValue=3.5*phValue;                      //convert the millivolt into pH value  
+  return phValue;
+}
+
+void EC_Initial(void)
+{
+  // analog pin does not need to set the mode.
+}
+float Get_EC_Val(void)
+{
+  unsigned char test_count=0;
+  unsigned int TotalsensorValue=0,average_EC=0;
+  float ECcurrent = 0.0;
+  for(test_count=0;test_count<10;test_count++){
+    TotalsensorValue = TotalsensorValue + analogRead(EC_SensorPin);
+    delay(100);
+  }
+  average_EC = (float)(TotalsensorValue)/2.048;
+  if(average_EC<=448) {ECcurrent=6.84*average_EC-64.32;} //1ms/cm<EC<=3ms/cm
+  else if(average_EC<=1457) {ECcurrent=6.98*average_EC-127;} //3ms/cm<EC<=10ms/cm
+  else {ECcurrent=5.3*average_EC+2278;} //10ms/cm<EC<20ms/cm
+  return ECcurrent/1000.0; // Unit:ms/cm
+}
+void TDS_Initial(void)
+{
+  gravityTds.setPin(EC_SensorPin);
+  gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();  //initialization
+}
+float Get_TDS_Val(void)
+{
+  gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+  gravityTds.update();  //sample and calculate 
+  return gravityTds.getTdsValue();  // then get the value
+}
+float SI7021_Get_Temp(void)
+{
+  byte buffer[] = {0, 0};
+  word outTemp = 0;
+  float valueTemp = 0;
+
+  Wire.beginTransmission(ADDRESS_SI7021);
+  Wire.write(READ_T_FROM_PRE_RH_MEASUREMENT);
+  Wire.endTransmission();
+
+  Wire.requestFrom(ADDRESS_SI7021, 2);
+  if(Wire.available() >= 2){
+    buffer[0] = Wire.read(); //high byte
+    buffer[1] = Wire.read(); //low byte; no crc  
+  }
+  outTemp = (buffer[0]<<8) | buffer[1];
+  valueTemp = 175.72*outTemp/65536 - 46.85;
+  return valueTemp;
+}
+
+float SI7021_Get_Humi(void)
+{
+  byte buffer[] = {0, 0, 0};
+  word outHumi = 0;
+  float valueHumi = 0;
+
+  Wire.beginTransmission(ADDRESS_SI7021);  
+  Wire.write(MEASURE_RH_HOLD);
+  Wire.endTransmission();
+
+  Wire.requestFrom(ADDRESS_SI7021, 3);
+  if(Wire.available() >= 3){
+    buffer[0] = Wire.read(); //high byte
+    buffer[1] = Wire.read(); //low byte
+    buffer[2] = Wire.read(); //crc
+  }
+  outHumi = (buffer[0]<<8) | buffer[1];
+  valueHumi = 125.0*outHumi/65536 - 6;
+  return valueHumi;
+}
+
+void CO2_USART_Initial(int CO2_baud)
+{
+  Serial2.begin(CO2_baud);
+}
+int CO2_USART_GetValue(void)
+{
+  unsigned char CO2_BUF[7] = "";
+  unsigned int H_Bits=0,L_Bits=0,H_Bits_H=0,H_Bits_L=0,L_Bits_H=0,L_Bits_L=0;
+  unsigned int CO2_PPM = 0;
+  Serial2.write(CMD_Array,8);
+  delayMicroseconds(5000);
+  while(Serial2.available() < 7) {;}
+  while(Serial2.available() != 0){Serial2.readBytes(CO2_BUF,7);}
+  
+  H_Bits = CO2_BUF[3];
+  L_Bits = CO2_BUF[4];
+  
+  H_Bits_H = H_Bits>>4;
+  H_Bits_L = H_Bits&0x0F;
+  L_Bits_H = L_Bits>>4;
+  L_Bits_L = L_Bits&0x0F;
+  CO2_PPM = H_Bits_H*4096 + H_Bits_L*256 + L_Bits_H*16 + L_Bits_L;
+  return CO2_PPM;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial3.begin(9600);
+  CO2_USART_Initial(9600);
+  DelayPin_initial(Delay_PinList,Delay_Device);
+  Wire.begin();
+  
+  pinMode(dirPin,OUTPUT);
+  pinMode(stepperPin,OUTPUT);
+  pinMode(enPin,OUTPUT);
+  pinMode(X_Trigger,INPUT);
+
+  pinMode(Pin_AP,OUTPUT);
+  pinMode(Pin_AN,OUTPUT);
+  pinMode(Pin_BP,OUTPUT);
+  pinMode(Pin_BN,OUTPUT);
+  pinMode(Y_Trigger,INPUT);
+  
+  // TDS_Initial();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial3.available() != 0){
+      Serial3.readBytes(Buffers,CMD_DATA_Len);
+      Update_Flag = 1;
+  }
+  
+  if(Update_Flag == 1){
+    if(Buffers[0] == 'A') {current_steps = int(float(Buffers[1])/x_mm_pp); x_step(1,current_steps);  Serial3.println("OK-A");} // 向右后退
+    if(Buffers[0] == 'B') {current_steps = int(float(Buffers[1])/x_mm_pp); x_step(0,current_steps);  Serial3.println("OK-B");} // 向左前进
+    if(Buffers[0] == 'C') {current_steps = int(float(Buffers[1])/y_mm_pp); y_step(1,current_steps);  Serial3.println("OK-C");} // 向下前进
+    if(Buffers[0] == 'D') {current_steps = int(float(Buffers[1])/y_mm_pp); y_step(0,current_steps);  Serial3.println("OK-D");} // 向上后退
+    if(Buffers[0] == 'E') {x_initial();  delay(300); Serial3.println("OK-E");}
+    if(Buffers[0] == 'F') {y_initial();  delay(300); Serial3.println("OK-F");}
+    if(Buffers[0] == 'S') {Stop_Motor(); delay(300); Serial3.println("OK-S");}
+    if(Buffers[0] == 'G') {digitalWrite(CO2_Control_Pin,LOW); delay(1000); Serial3.println("OK-GCO2 ON");}
+    if(Buffers[0] == 'H') {digitalWrite(CO2_Control_Pin,HIGH); delay(1000); Serial3.println("OK-HCO2 OFF");}
+    if(Buffers[0] == 'I') {Current_PH = Get_PH_Val(PH_SensorPin); delay(1000); Serial3.print(" PH:");Serial3.print(Current_PH,2); Serial3.println("OK-I");}
+    if(Buffers[0] == 'J') {Current_EC = Get_EC_Val(); delay(1000); Serial3.print(" EC:");Serial3.print(Current_EC,2);Serial3.println("OK-J");}
+    if(Buffers[0] == 'K') {Current_CO = CO2_USART_GetValue(); delay(1000); Serial3.print(" CO2:");Serial3.print(Current_CO,DEC);Serial3.println("OK-K");}
+    
+    // whiteLight_Pin,GorwLight1_Pin,GorwLight2_Pin,DosingPump1_Pin,DosingPump2_Pin,DosingPump3_Pin;
+    if(Buffers[0] == 'L') {DelayPin_Control(whiteLight_Pin,1);delay(1500); Serial3.println("OK-L");}
+    if(Buffers[0] == 'M') {DelayPin_Control(whiteLight_Pin,0);delay(1500); Serial3.println("OK-M");}
+    if(Buffers[0] == 'N') {DelayPin_Control(GorwLight1_Pin,1);delay(1500); Serial3.println("OK-N");}
+    if(Buffers[0] == 'O') {DelayPin_Control(GorwLight1_Pin,0);delay(1500); Serial3.println("OK-O");}
+    if(Buffers[0] == 'P') {DelayPin_Control(GorwLight2_Pin,1);delay(1500); Serial3.println("OK-P");}
+    if(Buffers[0] == 'Q') {DelayPin_Control(GorwLight2_Pin,0);delay(1500); Serial3.println("OK-Q");}
+    
+    if(Buffers[0] == 'R') {DelayPin_Control(DosingPump1_Pin,1);delay(1500); Serial3.println("OK-R");}
+    if(Buffers[0] == 'T') {DelayPin_Control(DosingPump1_Pin,0);delay(1500); Serial3.println("OK-T");}
+    if(Buffers[0] == 'U') {DelayPin_Control(DosingPump2_Pin,1);delay(1500); Serial3.println("OK-U");}
+    if(Buffers[0] == 'V') {DelayPin_Control(DosingPump2_Pin,0);delay(1500); Serial3.println("OK-V");}
+    if(Buffers[0] == 'W') {DelayPin_Control(DosingPump3_Pin,1);delay(1500); Serial3.println("OK-W");}
+    if(Buffers[0] == 'X') {DelayPin_Control(DosingPump3_Pin,0);delay(1500); Serial3.println("OK-X");}
+    if(Buffers[0] == 'Y') {Humi = SI7021_Get_Humi();delay(1500); Serial3.print(Humi,2);Serial3.println("OK-Y");}
+    if(Buffers[0] == 'Z') {Temp = SI7021_Get_Temp();delay(1500); Serial3.print(Temp,2);Serial3.println("OK-Z");}
+    Update_Flag = 0;
+  }
+}
---
+++ image_sample_pro/Raspberry_Arduino_Motor_Control/Raspberry_ttyAMA0_Arduino_Serial3_USBImageSample.py
@@ -0,0 +1,389 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+import serial
+import time
+import os 
+import cv2
+
+Mashine_Name = 'P1'
+print 'Mashine_Name:',Mashine_Name
+print 'Raspberry GPIO initial...'
+
+Camera_IR_Pin = 17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Camera_IR_Pin,GPIO.OUT)
+
+def rpi2_usart_open(device,baudrate):
+    return serial.Serial(device,baudrate)
+def rpi2_usart_send(serial,data):
+    # serial.flushOutput()
+    serial.write(data)
+    # serial.flushOutput()
+def rpi2_usart_recv(serial,lenght):
+    buffers = ''
+    while True:
+        FIFO_len = serial.inWaiting()
+        # print "Waiting data:",FIFO_len
+        if lenght <= FIFO_len:
+            buffers = serial.read(FIFO_len)
+            # serial.flushInput()
+            break
+    return buffers
+
+CO2_dev = "/dev/ttyAMA0"
+CO2_baud = 9600
+
+def CO2_USART_Initial(CO2_dev,CO2_baud):
+    return rpi2_usart_open(CO2_dev,CO2_baud)
+def CO2_USART_GetValue(CO2_ser,CO2_CMD):
+    CO2_BUF = ''
+    rpi2_usart_send(CO2_ser,CO2_CMD)
+    CO2_BUF = rpi2_usart_recv(CO2_ser,7)
+                            
+    H_Bits = ord(CO2_BUF[3])
+    L_Bits = ord(CO2_BUF[4])
+                            
+    H_Bits_H = H_Bits>>4
+    H_Bits_L = H_Bits&0x0F
+    L_Bits_H = L_Bits>>4
+    L_Bits_L = L_Bits&0x0F
+                        
+    return H_Bits_H*4096 + H_Bits_L*256 + L_Bits_H*16 + L_Bits_L
+
+# CO2_ser = CO2_USART_Initial(CO2_dev,CO2_baud) # Before you use the RPi-CO2 Sensor Please Initial the USART-ttyAMA0
+CO2_CMD = chr(254)+chr(4)+chr(0)+chr(3)+chr(0)+chr(1)+chr(213)+chr(197)
+
+x_n = 4
+y_n = 8
+
+x_distance = [0,58,100,100] # Unit:mm
+y_distance = [0,20,115,115,115,115,115,35] # [0,60,115,115,115,115,115,40]
+
+pre_min = 0
+min_update = 0
+
+print 'Initial the system...'
+
+x_axis_lable = 0
+y_axis_lable = 0
+
+Position = [['A1','B1','C1','D1'],['A2','B2','C2','D2'],['A3','B3','C3','D3']\
+        ,['A4','B4','C4','D4'],['A5','B5','C5','D5'],['A6','B6','C6','D6']\
+        ,['A7','B7','C7','D7'],['A8','B8','C8','D8']]
+
+
+Start_Date = time.localtime(time.time())
+
+if Start_Date.tm_mon >= 10:
+    Start_month = str(Start_Date.tm_mon)
+else:
+    Start_month = '0' + str(Start_Date.tm_mon)
+
+if Start_Date.tm_mday >= 10:
+    Start_day = str(Start_Date.tm_mday)
+else:
+    Start_day = '0' + str(Start_Date.tm_mday)
+
+Motor_CMD = dict({'x-right':'A','x-left':'B','y-down':'C','y-up':'D','x-init':'E','y-init':'F','stop':'S','Hold':'N'})
+System_CMD = dict({'CO2-ON':'G','CO2-OFF':'H'})
+print Motor_CMD,type(Motor_CMD),Motor_CMD['x-right']
+timeout_count = 0
+recv_data = 'N'
+recv_n = 0
+
+abs_path = "/home/pi/nexgen_pro/image_sample_pro/Raspberry_Arduino_Motor_Control/image_sample_Journal.log" # ADD-line
+
+def Journal_log(log,path='?'):
+    DefaultPath = "/home/pi/nexgen_pro/image_sample_pro/Raspberry_Arduino_Motor_Control/image_sample_Journal.log"
+    if path == '?':
+        path = DefaultPath
+    Journal = open(path,'a')
+    Journal.write(log)
+    Journal.close()
+def Get_time_str(separator,struct=''):
+    if struct == '':
+        time_struct_val = time.localtime(time.time())
+        return str(time_struct_val.tm_year) + separator + str(time_struct_val.tm_mon) + separator + str(time_struct_val.tm_mday)+ separator + str(time_struct_val.tm_hour) + separator + str(time_struct_val.tm_min) + separator + str(time_struct_val.tm_sec) 
+    return str(struct.tm_year) + separator + str(struct.tm_mon) + separator + str(struct.tm_mday)+ separator + str(struct.tm_hour) + separator + str(struct.tm_min) + separator + str(struct.tm_sec)
+
+Journal_log(log="################################## System Start:" + Get_time_str(':',Start_Date) + "###########################################\n")
+
+def ASK_Slave(serial,cmd_data):
+    global recv_data,recv_n
+    global abs_path
+    timeout_count = 0
+    timeout_total = 0
+    print 'recv_n',recv_n
+    print 'recv_data_B',recv_data
+    recv_n = 0
+    recv_data = 'N'
+    while True:
+        print "Motor is moving: ",timeout_count," times"
+        time.sleep(0.5)
+        USART_Status = 'BUSY'
+        inWaiting_count = 0
+        while USART_Status == 'BUSY':
+            try:
+                recv_n = serial.inWaiting()
+                USART_Status = 'FREE'
+                inWaiting_count = 0
+                pass
+            except Exception,e:
+                print 'USART Wrong!'
+                USART_Status = 'BUSY'
+                Journal_log(log=Get_time_str(':')+"-USART Error:"+str(Exception)+str(e)+'\n')
+                inWaiting_count += 1
+                if inWaiting_count > 3:
+                    inWaiting_count = 0
+                    print 'Goto Repeate CMD Send.'
+                    break
+                time.sleep(3)
+                pass
+        print cmd_data,'&& recv_n:',recv_n
+        if recv_n >= 2:
+            recv_data = serial.read(recv_n)
+            print recv_n,' && ',recv_data
+            serial.flushInput()
+            if 'OK' in recv_data and cmd_data[0] in recv_data:
+                print 'recv_data_A',recv_data,'+',recv_n
+                print 'Moving Finished.'
+                recv_data_return = recv_data
+                recv_n = 0
+                recv_data = 'N'
+                serial.flushOutput()
+                time.sleep(0.5) # 1
+                return recv_data_return
+            else:
+                print "Wrong Control,Break!"
+                recv_n = 0
+                recv_data = 'N'
+                serial.flushOutput()
+        timeout_count += 1
+        if timeout_count >= 30:
+            print "No response Slave,Repeat send cmd please."
+            serial.write(cmd_data)
+            timeout_total += timeout_count
+            timeout_count = 0
+            if timeout_total <= 60:
+                Journal_log(log="Slave No response for CMD:" + cmd_data[0] + " " + str(timeout_total) + " times,Repeat Send CMD...\n")
+            else:
+                Journal_log(log="Slave No response for CMD:" + cmd_data[0] + " " + str(timeout_total) + " times,Shutdown System...\n")
+                died_time = time.localtime(time.time())
+                Journal_log(log="System died at: "+ Get_time_str(':') +'.\n')
+                os.system('sync')
+                time.sleep(1)
+                print 'sudo shutdown -r now'
+                os.system('sudo shutdown -r now')
+
+def Test_Motor(serial):
+    print 'Testing Motor...'
+    CMD_Send = Motor_CMD['x-left'] + chr(0)
+    serial.write(CMD_Send)
+    time.sleep(1.5)
+    serial.write(Motor_CMD['x-init'])
+    time.sleep(2)
+    CMD_Send = Motor_CMD['y-down'] + chr(0)
+    serial.write(CMD_Send)
+    time.sleep(1.5)
+    serial.write(Motor_CMD['y-init'])
+    time.sleep(2)
+    print 'Testing Motor Finished.'
+
+def Motor_Control(serial,CMD_IN,distance):
+    global Motor_CMD
+    ser_state = 1
+    CMD_Res = "Error"
+    CMD_Send = CMD_IN + chr(distance) + '\0'
+    print CMD_Send
+    try:
+        if 'S' == CMD_Send[0]:
+            serial.write(Motor_CMD['stop'])
+            ASK_Slave(serial,Motor_CMD['stop'])
+            time.sleep(1.5)
+        else:
+            serial.write(CMD_Send)
+            CMD_Res = ASK_Slave(serial,CMD_Send)
+            time.sleep(1)
+            pass
+    except:
+            print "ser is wrong!"
+            Journal_log(log=Get_time_str(':')+"Motor_Control(ser," + CMD_Send + ") Func Failed\n")
+            ser_state = 0
+    time.sleep(1)
+    return CMD_Res,ser_state
+
+def Serial_Get():
+    USB_dev = ''
+    find_count = 0
+    while USB_dev == '':
+        dev_list = os.listdir('/dev/')
+        for dev in dev_list:
+            if 'ttyAMA' in dev: # If use the ttyUSBx device please change it to be 'USB'
+                USB_dev = dev
+                print 'USB device founded:',USB_dev
+                find_count = 0
+                break
+        find_count += 1
+        if find_count >= 30:
+            Journal_log(log=Get_time_str(':')+'-Error:Arduino Device Not Founded.Restart Arduino.\n')
+            print 'Restart the Arduino Power supply by raspberry\'s GPIO Directly.'
+            find_count = 0
+        time.sleep(1)
+    serial_dev = '/dev/' +  USB_dev
+    Serial_Status = 'BUSY'
+    while Serial_Status == 'BUSY':
+        try:
+            ser = serial.Serial(serial_dev, 9600)
+            Serial_Status = 'FREE'
+            print 'Serial Opened Successful.'
+            pass
+        except Exception,e:
+            print 'Serial Opend Failed!Try again.'
+            Serial_Status = 'BUSY'
+            Journal_log(log=Get_time_str(':')+'-Serial Error:'+str(Exception)+str(e)+'\n')
+            time.sleep(5)
+            pass
+    ser.flushInput()
+    ser.flushOutput()
+    return ser
+
+print 'System Start Monitor...'
+CO2_Status = '?'
+RPi_CO2 = False
+ser = Serial_Get()
+Motor_Control(ser,'M',0) # Close the whilt light.
+while True:
+    date = time.localtime(time.time())
+    if pre_min != date.tm_min:
+        print "minutes update..."
+        if date.tm_hour >= 8 and date.tm_hour < 19: # Judge the CO2 detect time. 8-19
+            print "Monitor CO2..."
+            if RPi_CO2:
+                CO2_PPM = CO2_USART_GetValue(CO2_ser,CO2_CMD) # Get the CO2-Values form Raspiberry<--->CO2_Sensor.
+            else:
+                #ser = Serial_Get()
+                CMD_Res = Motor_Control(ser,'K',0) # K CO2:493OK-KCO2
+                #ser.close()
+                if CMD_Res[1] == 0:
+                    ser.close()
+                    time.sleep(2)
+                    ser = Serial_Get()
+                    CO2_STR = "CO2:1500OK-KCO2" # the CO2 Value should bigger than 1200.
+                else:
+                    CO2_STR = CMD_Res[0]
+                CO2_Start_Index = CO2_STR.find(':') + 1
+                CO2_End_Index = CO2_STR.find('OK')
+                CO2_PPM = int(CO2_STR[CO2_Start_Index:CO2_End_Index])
+            print Get_time_str('-',date),':',CO2_PPM,'ppm'
+            if CO2_PPM < 1000: # Judge the CO2 ppm
+                if CO2_Status != 'G':
+                    #ser = Serial_Get()
+                    Motor_Control(ser,'G',0) # Control the CO2-Delay status:ON
+                    #ser.close()
+                    CO2_Status = 'G'
+            elif CO2_PPM > 1200:
+                if CO2_Status != 'H':
+                    #ser = Serial_Get()
+                    Motor_Control(ser,'H',0) # Control the CO2-Delay status:OFF
+                    CO2_Status = 'H'
+                    #ser.close()
+        else:
+            if CO2_Status != 'H':
+                #ser = Serial_Get()
+                Motor_Control(ser,'H',0) # Control the CO2-Delay status:OFF
+                CO2_Status = 'H'
+                #ser.close()
+        min_update = 1
+        pre_min = date.tm_min # >=8 <19
+    if date.tm_hour % 1 == 0 and date.tm_min % 59 == 0 and min_update and date.tm_hour >= 7 and date.tm_hour < 19: # 19
+        ser.close() # Add line
+        time.sleep(2) # Wait for Serial been closed.
+        ser = Serial_Get()
+        Motor_Control(ser,'H',0); # Control the CO2-Delay status:OFF
+        Motor_Control(ser,'L',0); # ON the whilte light.
+        Motor_Control(ser,'N',0); # OFF the GrowLight1.
+        Motor_Control(ser,'P',0); # OFF the GrowLight2.
+        CO2_Status = 'H'
+        if date.tm_hour > 19 or date.tm_hour < 8: # Set the Camera's IR Capture func.
+            GPIO.output(Camera_IR_Pin,False)
+            print 'IR mode is ON!'
+        else:
+            GPIO.output(Camera_IR_Pin,True)
+            print 'IR mode is OFF!'
+        Time_Start = time.time()
+        Time_NOW = Get_time_str('-',date)
+        Journal_log(log="Start sampling images:"+Time_NOW+'\n') # ADD-line
+        min_update = 0
+        print 'Current time:',Time_NOW
+        if date.tm_mon >= 10:
+            Current_month = str(date.tm_mon)
+        else:
+            Current_month = '0' + str(date.tm_mon)
+        if date.tm_mday >= 10:
+            Current_day = str(date.tm_mday)
+        else:
+            Current_day = '0' + str(date.tm_mday)
+        if date.tm_hour >= 10:
+            Current_hour = str(date.tm_hour)
+        else:
+            Current_hour = '0' + str(date.tm_hour)
+        if date.tm_min >= 10:
+            Current_min = str(date.tm_min)
+        else:
+            Current_min = '0' + str(date.tm_min)
+        
+        print 'image sampling...'
+        Test_Motor(ser)
+        for x in range(x_n):
+            Motor_Control(ser,Motor_CMD['x-left'],x_distance[x]) # X-axis Going ON Positive
+            for y in range(y_n):
+                if x%2 == 0 and y == 0:
+                    Motor_Control(ser,Motor_CMD['y-init'],0)
+                if x%2 == 0 and y != 0:
+                    Motor_Control(ser,Motor_CMD['y-down'],y_distance[y])
+                    y_axis_lable += 1
+                if x%2 != 0 and y != 0:
+                    Motor_Control(ser,Motor_CMD['y-up'],y_distance[-y])
+                    y_axis_lable -= 1
+                print x_axis_lable,y_axis_lable
+                path_prefix = "/home/pi/nexgen_pro/image_sample_pro/Raspberry_Arduino_Motor_Control/image_data/"
+                path = path_prefix + Position[y_axis_lable][x_axis_lable]+'/'+Mashine_Name+'_'+str(Start_Date.tm_year)+Start_month+Start_day+'_'+str(date.tm_year)+Current_month+Current_day+'_'+Current_hour+Current_min+'_'+Position[y_axis_lable][x_axis_lable]+'_V'+'_NU'+'.jpg'
+                print 'Camera initilize...'
+                if os.path.exists('/dev/video0') == False:
+                    print "No vodeo0 device!Try agine."
+                    Journal_log(log="Camera Device Not Exist.")
+                    sys.exit(2)
+                Camera_Status = 'BUSY'
+                cap = cv2.VideoCapture(0)
+                while Camera_Status == 'BUSY':
+                    print "Try USB-Camera..."
+                    try:
+                        ret,frame = cap.read()
+                        cv2.imwrite(path,frame)
+                        cap.release()
+                        print 'Sample Successfully.'
+                        Camera_Status = 'FREE'
+                        pass
+                    except Exception,e:
+                        print 'Camera Wrong!'
+                        Camera_Status = 'BUSY'
+                        Journal_log(log=Get_time_str(':') + "-USBCamera Wrong? "+str(Exception)+':'+str(e)+'\n')
+                        time.sleep(10)
+                        pass
+                Journal_log(log=path[83:]+'\n') # ADD-line
+                print path
+            x_axis_lable += 1
+        Motor_Control(ser,Motor_CMD['x-init'],0)
+        Motor_Control(ser,'M',0)
+        Motor_Control(ser,'O',0)
+        Motor_Control(ser,'Q',0)
+        # ser.close()
+        print "Serial been Closed now."
+        x_axis_lable = 0
+        y_axis_lable = 0
+        Time_End = time.time()
+        TimeCost = (Time_End-Time_Start)/60
+        print '################ Time cost: ',TimeCost,' minutes. ####################'
+        Journal_log(log="Sample Finished.(TimeCost:"+str(TimeCost)+" mins)\n")
